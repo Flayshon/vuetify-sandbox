6e26908ddc98d9e6113b5ac666f26375ac65e245
Flayshon <flayshon@gmail.com>
2020-03-06 00:17:21 -0300

    working setup with vuefify1+dayspan

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -6,13 +6,19 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
+import Vue from 'vue';
 import Vuetify from 'vuetify';
+import DaySpanVuetify from 'dayspan-vuetify';
+import App from './components/App.vue';
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
+Vue.config.productionTip = false
+
 Vue.use(Vuetify);
 
-import App from './components/App.vue'
-import DaySpanVuetify from 'dayspan-vuetify-2';
 Vue.use(DaySpanVuetify, {
     methods: {
         getDefaultEventColor: () => '#1976d2'
@@ -41,6 +47,5 @@ Vue.use(DaySpanVuetify, {
 
 const app = new Vue({
     el: '#app',
-    vuetify: new Vuetify(),
     render: h => h(App)
-}).$mount('#app');
+});
